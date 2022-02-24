7b19ecc78dcb860bff74642c97623e3154133893
Trygve Lie <trygve.lie@finn.no>
2022-02-24 16:56:54 +0100

    Add docker

--- npm-project/index.js
+++ npm-project/index.js
@@ -7,7 +7,7 @@ fastify.get('/', (request, reply) => {
   reply.send({ hello: 'world' })
 })
 
-fastify.listen(3000, (err, address) => {
+fastify.listen(3000, '0.0.0.0',(err, address) => {
   if (err) throw err
   // Server is now listening on ${address}
 })
